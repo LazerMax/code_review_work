65b072bf362896627a06fcb7117e7102e5bad5d8
nekrochan <rubus.saxatilis.99@gmail.com>
2024-05-11 23:44:54 +0300

    Forovskaya V.V.: ToDo-fields has been edited

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,26 +6,35 @@ class CalculatorTest {
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        Calculator calc = new Calculator();
+        assertEquals(2, calc.add(1,1));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        Calculator calc = new Calculator();
+        assertEquals(0, calc.dif(1,1));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        Calculator calc = new Calculator();
+        try {
+            assertEquals(5, calc.div(10,2));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        Calculator calc = new Calculator();
+        assertEquals(6, calc.times(2,3));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Calculator calc = new Calculator();
+        assertEquals(8, calc.solver());
     }
 }
